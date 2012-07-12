e673d05820b76cae9f4628db7a1bdde3eee37484
schlager <zarquon42@gmail.com>
2012-07-12 12:48:24 +0200

    added different options for closest point search in icp and gaussDisplace

--- R/gaussDisplace.r
+++ R/gaussDisplace.r
@@ -1,30 +1,38 @@
-gaussDisplace <- function(mesh1,mesh2,sigma,gamma=2,W0,f,oneway=F,k=1,nh=NULL,tol=0,cores=detectCores(),...)
+gaussDisplace <- function(mesh1,mesh2,sigma,gamma=2,W0,f,oneway=F,k=1,nh=NULL,tol=0,cores=detectCores(),pro=c("vcg","morpho"),...)
 {
+  pro <- substring(pro[1],1L,1L)
+  if (pro == "v")
+    {project3d <- vcgClost
+   }
+  if (pro == "m")
+    {
+      protmp <- function(x,y,sign=F)
+        {
+          out <- closemeshKD(x,y,k=10,sign=sign)
+          return(out)
+        }
+      project3d <- protmp
+    }
   out <- NULL
   t0 <- Sys.time()
   sigma0 <- sigma
   M0 <- t(mesh2$vb[1:3,])
   S0 <- t(mesh1$vb[1:3,])
   sigma <- (sigma0*f^(-k))^2
-  S <- vert2points(vcgClost(mesh1,mesh2,sign=F))
-  t1 <- Sys.time()
-    
-   if (oneway)
+  S <- vert2points(project3d(mesh1,mesh2,sign=F))
+  if (oneway)
     {
       M <- vert2points(mesh2)
     }
   else
     {
-      #M <-  projRead(vert2points(mesh2),mesh1,sign=F,smooth=F,ignore.stdout=F,readnormals=F)
-      M <- vert2points(vcgClost(mesh2,mesh1,sign=F))
-    }
-    t2 <- Sys.time()
+      M <- vert2points(project3d(mesh2,mesh1,sign=F))
+     }
+  
   if (!is.null (nh))
     {
-     
-          clostIndW <- mcNNindex(S,W0,k=nh,cores=cores,...)
-          clostIndP <- mcNNindex(M,W0,k=nh,cores=cores,...)
-      
+      clostIndW <- mcNNindex(S,W0,k=nh,cores=cores)
+      clostIndP <- mcNNindex(M,W0,k=nh,cores=cores)
     }
   t3 <- Sys.time()
   D1 <- S-S0
@@ -49,61 +57,69 @@ gaussDisplace <- function(mesh1,mesh2,sigma,gamma=2,W0,f,oneway=F,k=1,nh=NULL,to
     }
   mclist[[cores]] <-   list()
   mclist[[cores]][[1]] <- W0[-c(1:((cores-1)*iter)),]
-    mclist[[cores]][[2]] <- c(1:dim(W0)[1])[-c(1:((cores-1)*iter))]
-
-    tmpfun <- function(x,...)
-      {
-        tmp0 <- .Fortran("displace_mesh_gauss",x[[1]],nrow(x[[1]]),S0,nrow(S0),M,nrow(M),D1,D2,sigma,gamma,oneway,clostIndW[x[[2]],],nh,clostIndP[x[[2]],],tol=tol)[[1]]
-        return(tmp0)
-      }
-    tmp <- mclapply(mclist,tmpfun,mc.cores=cores)
-    for (i in 1:cores)
-      {
-        out <- rbind(out,tmp[[i]])
-      }
-  #addit <- W0+out
+  mclist[[cores]][[2]] <- c(1:dim(W0)[1])[-c(1:((cores-1)*iter))]
   
- # time <- system.time(out <- .Fortran("displace_mesh_gauss",W0,nrow(W0),S0,nrow(S0),M,nrow(M),D1,D2,sigma,gamma,oneway,clostIndW,nh,clostIndP,tol=tol))
+  tmpfun <- function(x,...)
+    {
+      tmp0 <- .Fortran("displace_mesh_gauss",x[[1]],nrow(x[[1]]),S0,nrow(S0),M,nrow(M),D1,D2,sigma,gamma,oneway,clostIndW[x[[2]],],nh,clostIndP[x[[2]],],tol=tol)[[1]]
+      return(tmp0)
+    }
+  tmp <- mclapply(mclist,tmpfun,mc.cores=cores)
+  
+  for (i in 1:cores)
+    {
+      out <- rbind(out,tmp[[i]])
+    }
+  ## time <- system.time(out <- .Fortran("displace_mesh_gauss",W0,nrow(W0),S0,nrow(S0),M,nrow(M),D1,D2,sigma,gamma,oneway,clostIndW,nh,clostIndP,tol=tol))
   #print(time)
   addit <- W0+out
   return(list(addit=addit))
 }
 
-gaussDisplMesh3d <- function(mesh1,mesh2,iterations=10,smooth=NULL,smoothit=10,smoothtype=c("taubin","laplace","HClaplace"),sigma=20,gamma=2,f=1.2,oneway=F,k=1,tol=0,lm1=NULL,lm2=NULL,icp=FALSE,icpiter=3,uprange=0.95,rhotol=1,nh=50,toldist=0,patch=NULL,repro=FALSE,cores=detectCores(),...)
+gaussDisplMesh3d <- function(mesh1,mesh2,iterations=10,smooth=NULL,smoothit=10,smoothtype=c("taubin","laplace","HClaplace"),sigma=20,gamma=2,f=1.2,oneway=F,tol=0,lm1=NULL,lm2=NULL,icp=FALSE,icpiter=3,uprange=0.95,rhotol=1,nh=50,toldist=0,patch=NULL,repro=FALSE,cores=detectCores(),pro=c("vcg","morpho"),...)
   {
+    rescue <- FALSE
     if (!is.null(patch))
       { ## append landmarks to meshes vertices
         mesh1$vb <- cbind(mesh1$vb,rbind(t(patch),1))
         colnames(mesh1$vb) <- NULL
         mdim <- dim(mesh1$vb)
         cols <- c((mdim[2]+1-dim(patch)[1]):mdim[2])
+        if (substr(smoothtype,1L,1L) %in% c("h","H") )
+          rescue <- TRUE
       }
     if (icp)
       {
         cat("performing icp matching\n")
         mesh1 <- icp(mesh1,mesh2,lm1=lm1,lm2=lm2,uprange=uprange,rhotol=rhotol,iterations=icpiter)
-        
       }
     cat("starting elastic matching\n****************\n")
     for (i in 1:iterations)
       {
         time0 <- Sys.time()
-        
         if (!is.null(smooth))
           { if (i %% smooth == 0)
               {
+                if(rescue)
+                  {
+                    tmppatch <- mesh1$vb[1:3,cols]
+                  }
                 cat("smoothing step\n")
                 mesh1 <- vcgSmooth(mesh1,type=smoothtype,iteration=smoothit)
-                 cat("smoothing finished\n")
+                cat("smoothing finished\n")
+                if (rescue)
+                  {
+                    mesh1$vb[1:3,cols] <- tmppatch 
+                  }
               }
           }
-            tmp <- gaussDisplace(mesh1,mesh2,sigma=sigma,gamma=gamma,f=f,W0=vert2points(mesh1),nh=nh,k=i,tol=toldist,cores=cores)
-            mesh1$vb[1:3,] <- t(tmp$addit)
-            if (!is.null(patch) && repro)
-              {
-                mesh1$vb[1:3,cols] <- vcgClost(t(mesh1$vb[1:3,cols]),mesh1)$vb[1:3,]
-              }
-          
+        tmp <- gaussDisplace(mesh1,mesh2,sigma=sigma,gamma=gamma,f=f,W0=vert2points(mesh1),nh=nh,k=i,tol=toldist,cores=cores,pro=pro)
+        mesh1$vb[1:3,] <- t(tmp$addit)
+        if (!is.null(patch) && repro)
+          {
+            mesh1$vb[1:3,cols] <- vcgClost(t(mesh1$vb[1:3,cols]),mesh1)$vb[1:3,]
+          }
+        
         time1 <- Sys.time()
         cat(paste("completed iteration",i, "in", time1-time0, "seconds\n"))
         cat("****************\n")
--- R/icp.r
+++ R/icp.r
@@ -1,16 +1,27 @@
-icp <- function(mesh1,mesh2,iterations=3,scale=T,lm1=NULL,lm2=NULL,uprange=0.9,rhotol=pi,k=50,reflection=FALSE)
+icp <- function(mesh1,mesh2,iterations=3,scale=T,lm1=NULL,lm2=NULL,uprange=0.9,rhotol=pi,k=50,reflection=FALSE,pro=c("vcg","morpho"))
   {
-
+    pro <- substring(pro[1],1L,1L)
+    if (pro == "v")
+    {project3d <- vcgClost
+   }
+  if (pro == "m")
+    {
+      tmpfun <- function(x,y,sign=F)
+        {
+          out <- closemeshKD(x,y,k=k,sign=sign)
+          return(out)
+        }
+      project3d <- tmpfun
+    }
     if (!is.null(lm1))## perform initial rough registration
       {
         mesh1 <- rotmesh.onto(mesh1,lm1,lm2,scale=T,reflection=reflection)$mesh
-        
       }
     
-    
     for( i in 1:iterations)
-      { cat("*")
-        proMesh <- vcgClost(mesh1,mesh2,sign=F) ## project mesh1 onto mesh2
+      {
+        cat("*")
+        proMesh <- project3d(mesh1,mesh2,sign=F) ## project mesh1 onto mesh2
         x1 <- vert2points(mesh1)
         x2 <- vert2points(proMesh)
         x2norm <- proMesh$normals
@@ -29,12 +40,11 @@ icp <- function(mesh1,mesh2,iterations=3,scale=T,lm1=NULL,lm2=NULL,uprange=0.9,r
         x1 <- x1[goodnorm,]
         x2 <- x2[goodnorm,]
         dists <- dists[goodnorm]
-
+        
         ## check distances of remaining points and select points
         qud <- quantile(dists,probs=uprange)
         good <- which(dists < qud)
         mesh1 <- rotmesh.onto(mesh1,x1[good,],x2[good,],scale=scale)$mesh
-        
       }
     cat("\n")
     return(mesh1)
@@ -43,15 +53,14 @@ icp <- function(mesh1,mesh2,iterations=3,scale=T,lm1=NULL,lm2=NULL,uprange=0.9,r
 
 normcheck <- function(mesh1,mesh2)
   {
-    
-     x1norm <- mesh1$normals
-     x2norm <- mesh2$normals
-     ln <- dim(x2norm)[[2]]
-     normcheck <- rep(0,dim(x2norm)[2])
-     storage.mode(normcheck) <- "double"
-     storage.mode(x1norm) <- "double"
-     storage.mode(x2norm) <- "double"
-     storage.mode(ln) <- "integer"
-     normcheck <- .Fortran("angcheck",x1norm,ln,x2norm,normcheck)[[4]]
-     return(normcheck)
-   }
+    x1norm <- mesh1$normals
+    x2norm <- mesh2$normals
+    ln <- dim(x2norm)[[2]]
+    normcheck <- rep(0,dim(x2norm)[2])
+    storage.mode(normcheck) <- "double"
+    storage.mode(x1norm) <- "double"
+    storage.mode(x2norm) <- "double"
+    storage.mode(ln) <- "integer"
+    normcheck <- .Fortran("angcheck",x1norm,ln,x2norm,normcheck)[[4]]
+    return(normcheck)
+  }
