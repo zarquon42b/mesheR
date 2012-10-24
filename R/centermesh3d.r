f295b6971a15ff4a089d43242b5cba8ac4463b2c
zarquon42b <zarquon42@gmail.com>
2012-10-24 15:08:26 +0200

    added centermesh3d to translate the barycenter of a mesh to origin or any arbitrary coordinate.

---
+++ R/centermesh3d.r
@@ -0,0 +1,12 @@
+centermesh3d <- function(x,center=NULL)
+  {
+
+    bary <- apply(vert2points(x),2,mean)
+    if (is.null(center))
+     center <- c(0,0,0)
+     
+       
+    x$vb[1:3,] <- x$vb[1:3,]- (bary-center)
+    invisible(x)
+  }
+ 
