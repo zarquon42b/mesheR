a3c2c5bd87fb26d47d792e95e17a03decf64773f
zarquon42b <zarquon42@gmail.com>
2013-03-14 13:22:28 +0100

    added option 'explode' and 'center' to rmInternals

--- R/rmInternals.r
+++ R/rmInternals.r
@@ -1,6 +1,15 @@
-rmInternals <- function(mesh,mindist=NULL)
+rmInternals <- function(mesh,mindist=NULL,explode=FALSE,center=NULL)
   {
-    meshoff <- meshOffset(mesh,0.05)
+
+    
+    if (explode)
+      {
+        if (is.null(center))
+          center <- apply(meshcube(mesh),2,mean)
+        mesh$normals[1:3,] <- mesh$vb[1:3,]-center
+        mesh$normals[1:3,] <- apply(mesh$normals[1:3,],2,function(x){x <- x/(sqrt(sum(x^2)))})
+      }
+    meshoff <- meshOffset(mesh,0.005)
     check <- vcgIntersect(meshoff,mesh)
     if (is.null(mindist))
       intern <- which(as.logical(check$quality))
