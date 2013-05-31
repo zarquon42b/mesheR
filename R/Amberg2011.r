c8f6e847e5986237980e71cdb1ba36b443ad6ff2
zarquon42b <zarquon42@gmail.com>
2013-05-31 15:11:55 +0200

    finished most of the routines to calculate S in Amberg2011

--- R/Amberg2011.r
+++ R/Amberg2011.r
@@ -1,4 +1,4 @@
-edgeSelectAmberg <- function(mesh)
+buildAffineAmberg <- function(mesh)
     {
         verts <- vert2points(mesh)
         norms <- t(facenormals(mesh)$normals[1:3,])##calculate face normals
@@ -8,7 +8,7 @@ edgeSelectAmberg <- function(mesh)
         rowind2 <- rowind3-1
         rowind1 <- rowind3-2
         whichsel <- cbind(rowind1,rowind2,rowind3,t(mesh$it),nrow(verts)+rowind)
-        sel <- Matrix(0,3*ncol(mesh$it),nrow(allcoo))
+        sel <- Matrix::Matrix(0,3*ncol(mesh$it),nrow(allcoo))
         sel[whichsel[,c(1,4)]] <- -1
         sel[whichsel[,c(1,5)]] <- 1
         sel[whichsel[,c(2,4)]] <- -1
@@ -19,7 +19,10 @@ edgeSelectAmberg <- function(mesh)
 
 mesh2sparse2 <- function (mat) 
 {
+    mat <- as.matrix(mat)
     nvb <- nrow(mat)
+    data <- t(mat)
+    data <- as.vector(data)
     j <- matrix(1:(nvb),3,nvb/3)
     j1 <- cbind(j,j,j)*0
     j1[,c(1:ncol(j))*3] <- j
@@ -27,23 +30,35 @@ mesh2sparse2 <- function (mat)
     j1[,(c(1:ncol(j))*3)-2] <- j
     j1 <- as.vector(j1)
     i1 <- as.vector(rbind(1:(nvb),1:(nvb),1:(nvb))[,1:nvb])
-    out <- sparseMatrix(j = j1, i = i1, x = as.vector(t(mat)))
-    invisible(t(out))
+    out <- Matrix::sparseMatrix(j = j1, i = i1, x = data)
+    invisible((out))
 }
-invertBD <- function(bd,cores=detectCores())
-{
-    matfun <- function(x,bd)
-        {
-            out <- t(solve(as.matrix(bd[((x*3)+1):((x*3)+3),((x*3)+1):((x*3)+3)])))
-            return(out)
-        }
-    ## for(i in 0:(ncol(bd)/3-1)){matlist[[i+1]] <- as.matrix(bd[((i*3)+1):((i*3)+3),((i*3)+1):((i*3)+3)])}
-    la <- as.list(0:(ncol(bd)/3-1))
-    solvit <- mclapply(la,matfun,bd,mc.cores=cores)
-    ##solvit <- mclapply(matlist,solve,mc.cores = cores)
-    ##solvit <- mclapply(solvit,t,mc.cores = cores)
-    solvemat <- matrix(unlist(solvit),ncol(bd),3,byrow = T)
-    solvemat <- mesh2sparse2(solvemat)
-    return(solvemat)
-}
+
+createArcNode <- function(mesh)
+    {
+        edges <- vcgNonBorderEdge(mesh)
+        n <- dim(edges)[1]
+        dimit <- ncol(mesh$it)
+        arcnode <- Matrix(0,n,dimit)
+        edges$ind <- 1:n
+        arcnode[cbind(edges$ind,edges$face1)] <- -1
+        arcnode[cbind(edges$ind,edges$face2)] <- 1
+        invisible(arcnode)
+    }
+
+createS <- function(mesh)
+    {
+        out <- list()
+        sel <- buildAffineAmberg(mesh)
+        transel <- sel$sel %*% sel$allcoo
+        invtransel <- multisolve3(transel,trans=FALSE)
+        out$sparseAijk <- mesh2sparse2(invtransel)
+        out$sel <- sel
+        dia3 <- sparseMatrix(i=1:3,j=1:3,x=rep(1,3))
+        arcnodes <- createArcNode(mesh) 
+        arcnodes <- kronecker(arcnodes,dia3)
+        out$process <- arcnodes%*%out$sparseAijk
+        return(out)
+    }
+
 
---
+++ R/multisolve.r
@@ -0,0 +1,27 @@
+multisolve3 <- function(A,trans=FALSE)
+{
+    A <- as.matrix(A)
+    n <- ncol(A)
+    m <- nrow(A)
+    l <- (m/n)-1
+    if (m %% n !=0)
+        stop("number of rows must be multiple of columns")
+    storage.mode(A) <- "double"
+    storage.mode(m) <- "integer"
+    storage.mode(n) <- "integer"
+    storage.mode(l) <- "integer"
+    trans <- as.integer(trans)
+    out <- .Fortran("multisolve3",A,m,l,trans)
+    return(out[[1]])
+}
+
+testsolve3 <- function(A)
+    {
+        A <- as.matrix(A)
+         n <- ncol(A)
+         storage.mode(n) <- "integer"
+         storage.mode(A) <- "double"
+         B <- A
+         out <- .Fortran("M33INV",A,A,TRUE)
+         return(out)
+     }
