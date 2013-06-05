447304adb58b97d4221517ac2149af0f1df20200
zarquon42b <zarquon42@gmail.com>
2013-06-05 15:10:06 +0200

    added options useiter and non-declaration of landmarks in AmbergRegister

--- R/AmbergRegister.r
+++ R/AmbergRegister.r
@@ -1,41 +1,60 @@
-AmbergRegister <- function(mesh1,mesh2,lm1,lm2,k=1,lambda=1,iterations=15,rho=pi/2,dist=2,border=F,smooth=T,tol=1e-4)
+AmbergRegister <- function(mesh1,mesh2,lm1=NULL,lm2=NULL,k=1,lambda=1,iterations=15,rho=pi/2,dist=2,border=F,smooth=T,tol=1e-4,useiter=FALSE)
     {
         mesh1 <- rmUnrefVertex(mesh1)
-        mesh1rot <- rotmesh.onto(mesh1,lm1,lm2)
-        lm1 <- mesh1rot$yrot
-        mesh1 <- mesh1rot$mesh
-        lmtmp1 <- lm1
-        lmtmp2 <- lm2
-        verts0 <- vert2points(mesh1)
-        
+        meshbord <- vcgBorder(mesh2)
+        count <- 0
         if (iterations < 1)
             iterations <- 1e10
         if (length(lambda) == 1)
             lambda <- rep(lambda,iterations)
-        
         else if (length(lambda) != iterations)
             stop("lambda must be vector of length 'iterations'")
-        cat(paste("-> performing iteration 1\n"))
-        tmp <- AmbergDeformSpam(mesh1,lmtmp1,lmtmp2,k0=k,lambda=lambda[1])
+        meshorig <- mesh1
+        if (!is.null(lm1) && !is.null(lm2))
+            {
+                mesh1rot <- rotmesh.onto(mesh1,lm1,lm2)
+                lm1 <- mesh1rot$yrot
+                meshorig <- mesh1 <- mesh1rot$mesh
+                lmtmp1 <- lm1
+                lmtmp2 <- lm2
+                verts0 <- vert2points(mesh1)
+                cat(paste("-> performing iteration 1\n"))
+                tmp <- AmbergDeformSpam(mesh1,lmtmp1,lmtmp2,k0=k,lambda=lambda[1])
+                count <- count+1
+            }
+        else
+            {
+                tmp <- list()
+                tmp$mesh <- mesh1
+                tmp$S <- createS(mesh1)
+                verts0 <- vert2points(mesh1)
+            }
+        
+        
         if (iterations > 1)
             {
                 error <- 1e12
-                count <- 2
+                count <- count+1
                 while (count <= iterations && error > tol)
                     {
+                        if (useiter)
+                            {
+                                verts0 <- vert2points(tmp$mesh)
+                                mesh1 <- tmp$mesh
+                            }
                         vert_old <- vert2points(tmp$mesh)
                         cat(paste("-> performing iteration",count,"\n"))
                         clost <- closemeshKD(tmp$mesh,mesh2)
                         verts1 <- vert2points(clost)
+
                         nc <- normcheck(clost,tmp$mesh)                        
 
                         ## find valid hits
                         normgood <- as.logical(nc < rho)
                         distgood <- as.logical(abs(clost$quality) <= dist)
                         bordergood <- 1
-                        if (border)
+                        if (!border)
                             {
-                                meshbord <- vcgBorder(mesh2)
                                 bordgood <- as.logical(!meshbord$borderit[clost$ptr])
                             }
                         
@@ -54,8 +73,11 @@ AmbergRegister <- function(mesh1,mesh2,lm1,lm2,k=1,lambda=1,iterations=15,rho=pi
                         lmtmp1 <- verts0[good,]
                         lmtmp2 <- verts1[good,]
                         ## map it according to new reference points
-                        
+                        #points3d(lmtmp2,col=count)
+                        if (useiter)
+                            tmp$S <- NULL
                         tmp <- AmbergDeformSpam(mesh1,lmtmp1,lmtmp2,k0=k,lambda=lambda[count],S=tmp$S)
+                        #oo <- wire3d(tmp$mesh,col=count)
                         gc()
                         ## calculate error
                         if (smooth)
@@ -68,5 +90,5 @@ AmbergRegister <- function(mesh1,mesh2,lm1,lm2,k=1,lambda=1,iterations=15,rho=pi
 
                     }
             }
-        return(list(mesh=tmp$mesh,meshrot=mesh1,lm1rot <- lm1))
+        return(list(mesh=tmp$mesh,meshrot=meshorig,lm1rot=lm1,lmtmp1=lmtmp1,lmtmp2=lmtmp2))
     }
