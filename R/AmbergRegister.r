f53c240d4bae886e72dd04b5f022973369bdde36
zarquon42b <zarquon42@gmail.com>
2013-06-12 15:39:49 +0200

    added option icp in AmbergRegister to perform initial icp matching.

--- R/AmbergRegister.r
+++ R/AmbergRegister.r
@@ -1,4 +1,4 @@
-AmbergRegister <- function(mesh1,mesh2,lm1=NULL,lm2=NULL,k=1,lambda=1,iterations=15,rho=pi/2,dist=2,border=FALSE,smooth=TRUE,tol=1e-4,useiter=TRUE,minclost=50,distinc=1)
+AmbergRegister <- function(mesh1,mesh2,lm1=NULL,lm2=NULL,k=1,lambda=1,iterations=15,rho=pi/2,dist=2,border=FALSE,smooth=TRUE,tol=1e-4,useiter=TRUE,minclost=50,distinc=1,icp=NULL)
     {
         mesh1 <- rmUnrefVertex(mesh1)
         meshbord <- vcgBorder(mesh2)
@@ -19,17 +19,29 @@ AmbergRegister <- function(mesh1,mesh2,lm1=NULL,lm2=NULL,k=1,lambda=1,iterations
         stopit <- FALSE
         if (!is.null(lm1) && !is.null(lm2))
             {   ## case: landmarks are provided
-                mesh1rot <- rotmesh.onto(mesh1,lm1,lm2)
-                lm1 <- mesh1rot$yrot
-                meshorig <- mesh1 <- mesh1rot$mesh
-                lmtmp1 <- lm1
-                lmtmp2 <- lm2
+                if (!is.null(icp))##perform initial icp-matching
+                    {
+                        meshorig <- mesh1 <- icp(mesh1,mesh2,lm1=lm1,lm2=lm2,iterations=icp[1],rhotol=icp[2],uprange=icp[3],scale=icp[4])
+                        tmp <- list()
+                        tmp$mesh <- mesh1
+                        if (!useiter)
+                            tmp$S <- createS(mesh1)
+                        verts0 <- vert2points(mesh1)
+                                            }
+                else
+                    {
+                        mesh1rot <- rotmesh.onto(mesh1,lm1,lm2)
+                        lm1 <- mesh1rot$yrot
+                        meshorig <- mesh1 <- mesh1rot$mesh
+                        lmtmp1 <- lm1
+                        lmtmp2 <- lm2
+                        cat(paste("-> performing landmark based matching 1\n"))
+                        tmp <- AmbergDeformSpam(mesh1,lmtmp1,lmtmp2,k0=k[1],lambda=lambda[1])
+                        count <- count+1
+                        if (iterations == 1)
+                            stopit <- TRUE
+                    }
                 verts0 <- vert2points(mesh1)
-                cat(paste("-> performing landmark based matching 1\n"))
-                tmp <- AmbergDeformSpam(mesh1,lmtmp1,lmtmp2,k0=k[1],lambda=lambda[1])
-                count <- count+1
-                if (iterations == 1)
-                    stopit <- TRUE
             }
         else
             {   ## case: meshes are already aligned
@@ -39,7 +51,6 @@ AmbergRegister <- function(mesh1,mesh2,lm1=NULL,lm2=NULL,k=1,lambda=1,iterations
                     tmp$S <- createS(mesh1)
                 verts0 <- vert2points(mesh1)
             }
-        
         
         if (!stopit)
             {
@@ -86,11 +97,11 @@ AmbergRegister <- function(mesh1,mesh2,lm1=NULL,lm2=NULL,k=1,lambda=1,iterations
                         lmtmp1 <- verts0[good,]
                         lmtmp2 <- verts1[good,]
                         ## map it according to new reference points
-                        #points3d(lmtmp2,col=count)
+                                        #points3d(lmtmp2,col=count)
                         if (useiter)
                             tmp$S <- NULL
                         tmp <- AmbergDeformSpam(mesh1,lmtmp1,lmtmp2,k0=k[count],lambda=lambda[count],S=tmp$S)
-                        #oo <- wire3d(tmp$mesh,col=count)
+                                        #oo <- wire3d(tmp$mesh,col=count)
                         gc()
                         ## calculate error
                         if (smooth)
@@ -102,7 +113,6 @@ AmbergRegister <- function(mesh1,mesh2,lm1=NULL,lm2=NULL,k=1,lambda=1,iterations
                         if (error < tol)
                             cat(paste("***\n==> Convergence threshold reached after",count,"iterations\n"))
                         count <- count+1
-
                     }
             }
         return(list(mesh=tmp$mesh,meshrot=meshorig,lm1rot=lm1,lmtmp1=lmtmp1,lmtmp2=lmtmp2))
