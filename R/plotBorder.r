e857c3f7d558b0a0dd9494b2b29a0e1baf0658f6
zarquon42b <zarquon42@gmail.com>
2013-06-24 10:49:06 +0200

    make sure to plot requested color for colorised meshes in plotBorder

--- R/plotBorder.r
+++ R/plotBorder.r
@@ -4,5 +4,6 @@ plotBorder <- function(mesh,col=2,lwd=2)
         bord <- which(edges$border == 1)
         bordmesh <- mesh
         bordmesh$it <- rbind(edges$vert1[bord],edges$vert2[bord],edges$vert1[bord])
+        bordmesh$material$color <- NULL
         wire3d(bordmesh,col=col,lwd=lwd,lit=F)
     }
