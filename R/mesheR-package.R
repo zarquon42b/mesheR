af3a74c911dcbbd8e3bb0d243efe30d7ebd69e9f
Stefan Schlager <zarquon42@gmail.com>
2013-06-28 17:14:23 +0200

    added mesheR roxygen file

---
+++ R/mesheR-package.R
@@ -0,0 +1,19 @@
+#' Meshing tools
+#' 
+#' A toolbox for meshing operations: elastic mesh mapping, Iteratively closest point matching, vertex selection, ...,
+#' 
+#' \tabular{ll}{ Package: \tab mesheR\cr Type: \tab Package\cr Version: \tab
+#' 0.3\cr Date: \tab 2013-06-28\cr License: \tab GPL\cr LazyLoad: \tab
+#' yes\cr }
+#' 
+#' @name mesheR-package
+#' @aliases mesheR-package mesheR
+#' @docType package
+#' @author Stefan Schlager
+#' 
+#' Maintainer: Stefan Schlager <zarquon42@@gmail.com>
+#' @references To be announced
+#' @keywords package
+#' @useDynLib mesheR
+
+NULL
