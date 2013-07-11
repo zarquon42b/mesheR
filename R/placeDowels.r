c584338a13a5e207632e6284c3173156541da71f
zarquon42b <zarquon42@gmail.com>
2013-07-11 10:53:33 +0200

    added options smooth and dowelcol in placeDowels and replaced closemeshKD with vcgClost

--- R/placeDowels.r
+++ R/placeDowels.r
@@ -11,6 +11,8 @@
 #' @param meshcol mesh color
 #' @param output if TRUE, a list containing triangular meshs is returned
 #' @param fine integer: amount of vertices to generate the cylinders representing tissue thickness.
+#' @param smooth logical: use smoothed normals for dowel orientation
+#' @param dowelcol specify dowelcolor.
 #'
 #' @details For orientation of the dowels, the (angle weighted) normal vectors of the surface is used. 
 #' @return if output = TRUE, a list containing mesh representations of placed dowels
@@ -21,13 +23,14 @@
 #' placeDowels(lms, humface, rep(5, 4),output = FALSE)
 #' @seealso \code{\link{cylinder}}
 #' @export placeDowels
-placeDowels <- function(lm, mesh, ldowel, render=TRUE,col=1,radius=1,meshcol=3,output=TRUE,fine=50)
+placeDowels <- function(lm, mesh, ldowel, render=TRUE,col=1,radius=1,meshcol=3,output=TRUE,fine=50, smooth=TRUE, dowelcol=NULL)
     {
 
         ldo <- FALSE
         colvec <- FALSE
-        projLM <- closemeshKD(lm,mesh)
-        ##projLM <- projRead(lm,mesh,smooth=smooth)
+        projLM <- vcgClost(lm,mesh,smooth=smooth)
+                                        ##projLM <- closemeshKD(lm,mesh)
+        #projLM <- projRead(lm,mesh,smooth=TRUE)
         if (length(ldowel) > 1)
             ldo <- TRUE
                   
@@ -44,6 +47,9 @@ placeDowels <- function(lm, mesh, ldowel, render=TRUE,col=1,radius=1,meshcol=3,o
                     ltmp=ldowel
 
                 dowels[[i]] <- cylinder(projLM$vb[1:3,i],projLM$normals[1:3,i],length=ltmp,radius=radius,fine=fine,adNormals=FALSE)
+                if (!is.null(dowelcol))
+                    dowels[[i]]$material$color <- matrix(dowelcol,dim(dowels[[i]]$it)[1],dim(dowels[[i]]$it)[2])
+                    
                 if (render)
                     {
                         if (colvec)
