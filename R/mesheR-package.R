a85fa5f1ca780c5d9a87ac9286c5b6536beb6a12
zarquon42b <zarquon42@gmail.com>
2013-11-06 16:18:19 +0100

    updated version including date suffix

--- R/mesheR-package.R
+++ R/mesheR-package.R
@@ -5,8 +5,8 @@
 #' \tabular{ll}{
 #' Package: \tab mesheR\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.4-00\cr
-#' Date: \tab 2013-07-19\cr
+#' Version: \tab 0.4.131106\cr
+#' Date: \tab 2013-11-06\cr
 #' License: \tab GPL\cr
 #' LazyLoad: \tab yes\cr }
 #' 
