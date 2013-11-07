313f49ed7dfc13b506fadf27889e0cd164e22185
zarquon42b <zarquon42@gmail.com>
2013-11-07 15:10:14 +0100

    updated daily version

--- R/mesheR-package.R
+++ R/mesheR-package.R
@@ -5,8 +5,8 @@
 #' \tabular{ll}{
 #' Package: \tab mesheR\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.4.131106\cr
-#' Date: \tab 2013-11-06\cr
+#' Version: \tab 0.4.131107\cr
+#' Date: \tab 2013-11-07\cr
 #' License: \tab GPL\cr
 #' LazyLoad: \tab yes\cr }
 #' 
