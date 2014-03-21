06416522e07c627c040db52d0ef20efa51bbed8d
zarquon42b <zarquon42@gmail.com>
2014-03-21 15:08:08 +0100

    added h5model.read

--- R/h5model.r
+++ R/h5model.r
@@ -1,6 +1,6 @@
 #' @export
 h5model.write <- function(model,filename) {
-    mean <- model$mshape
+    mean <- as.vector(t(model$mshape))
     pcaBasis <- model$PCs
     pcaVariance <- model$Variance[,1]
     h5 <- H5File(filename,'w')
@@ -8,5 +8,15 @@ h5model.write <- function(model,filename) {
     createH5Dataset(modgroup,"mean",mean)
     createH5Dataset(modgroup,"pcaBasis",pcaBasis)
     createH5Dataset(modgroup,"pcaVariance",pcaVariance)
+    
+}
+h5model.read <- function(filename) {
+    h5 <- H5File(filename)
+    model <- list()
+    mshape <- getH5Dataset(getH5Group(h5, "model"), "mean")[]
+    model$mshape <- matrix(mshape,length(mshape),3,byrow = T)
+    model$Variance <- getH5Dataset(getH5Group(h5, "model"), "pcaVariance")[]
+    model$PCs <- getH5Dataset(getH5Group(h5, "model"), "pcaBasis")[]
+    return(model)
     
 }
