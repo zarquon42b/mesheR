ba4072f2518a422d3d33e826a0fa0c94b1689ca9
schlager <zarquon42@gmail.com>
2014-10-10 16:39:24 +0200

    require shrinkfun in createBayes to be a two-argument function

--- R/createBayes.r
+++ R/createBayes.r
@@ -6,13 +6,13 @@
 #' @param sdmax numeric or vector of numerics (length must be of value =< \code{iterations} as to be specified in gaussMatch or AmbergRegister. If \code{length(sd) =n < iterations}, then the first n iterations will be restricted to the according standard deviations
 #' @param mahalanobis parameter according to mahaprob in function PredictSample from RvtkStatismo
 #' @param ptValueNoise parameter according to ptValueNoise in function statismoConstrainModel from RvtkStatismo
-#' @param wt weight put on the closest model configuration. E.g. if wt = 2 the deformation will be 0.33*closestPoints + 0.66*most probable model shape.
+#' @param wt numeric: a weight put on the closest model configuration. E.g. if wt = 2 the deformation will be 0.33*closestPoints + 0.66*most probable model shape. This can either be a constant scalar (that might be modified in each iteration by a shrinkage function - see below) or a vector of length sdmax.
 #' @param align logical: if the target mesh is already aligned to the model, align can be set to FALSE.
-#' @param shrinkfun a recursive function for the generation of the weights. See examples below.
+#' @param shrinkfun a two-valued function for the generation of the weight in the i-th iteration. See examples below.
 #' @return returns a list of class "BayesDeform"
 #' @examples
 #' \dontrun{
-#' shrinkfun <- function(x){x <- x*0.9; return(x)}
+#' shrinkfun <- function(x,i){x <- x*0.9^i; return(x)}
 #' # for wt=0.5 this will produce 
 #' # 
 #' }
@@ -26,7 +26,7 @@ createBayes <- function(model, sdmax=numeric(0),mahalanobis="chisq",ptValueNoise
     Bayes$wt <- rep(wt,length(sdmax))
     if (!is.null(shrinkfun)) {
         for (i in 2:length(sdmax))
-            Bayes$wt[i] <- shrinkfun(Bayes$wt[i-1])
+            Bayes$wt[i] <- shrinkfun(Bayes$wt[i],i)
     }
         
     Bayes$align <- align
