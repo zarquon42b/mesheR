4fb0c340d0dd0abfcf6b6e358908a1de56ad2e1d
zarquon42b <zarquon42@gmail.com>
2015-05-04 17:04:02 +0200

    added function meanMeshes

---
+++ R/meanMeshes.r
@@ -0,0 +1,25 @@
+#' average the vertices from a set of corresponding meshes
+#'
+#' average the vertices from a set of corresponding meshes
+#' @param ... meshes of class mesh3d
+#' @return a mesh with averaged vertices
+#' @export
+meanMeshes <- function(...) {
+    args <- list(...)
+    mmmvert <- RvtkStatismo::meshlist2array(args)
+    mmmvertmean <- Morpho::arrMean3(mmmvert)
+    ref <- args[[1]]
+    ref$vb[1:3,] <- t(mmmvertmean)
+    ref <- Rvcg::vcgUpdateNormals(ref)
+    return(ref)
+}
+meshlist2array <- function (meshlist) {
+    n <- length(meshlist)
+    k <- ncol(meshlist[[1]]$vb)
+    vertarr <- array(NA, dim = c(k, 3, n))
+    for (i in 1:n) vertarr[, , i] <- vert2points(meshlist[[i]])
+    dimnames(vertarr)[[3]] <- names(meshlist)
+    if (is.null(names(meshlist))) 
+        dimnames(vertarr)[[3]] <- paste("specimen", 1:n, sep = "_")
+    return(vertarr)
+}
