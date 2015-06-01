88af7250583ab959e548f3d526abbe71ccb6bf43
zarquon42b <zarquon42@gmail.com>
2015-06-01 12:02:22 +0200

    NEW: added getVertColor and setVertColor

--- R/colorMesh.r
+++ R/colorMesh.r
@@ -22,3 +22,36 @@ colorMesh <- function(mesh, col) {
     mesh$material <- material
     invisible(mesh)
 }
+
+#' get the per-vertex colors of an object of class mesh3d
+#'
+#' get the per-vertex colors of an object of class mesh3d
+#' @param x mesh3d
+#' @return vector of hexadecimal rgb values
+#' @export
+getVertColor <- function(x) {
+    if(is.null(x$material$color))
+        stop("mesh has no per-vertex color")
+    col <- 1:ncol(x$vb)
+    tmp1 <- data.frame(it=as.vector(x$it))
+    tmp1$rgb <- as.vector(x$material$color)
+    tmp1 <- unique(tmp1)
+    col[tmp1$it] <- tmp1$rgb
+    return(col)
+}
+
+#' set the per-vertex colors of an object of class mesh3d
+#'
+#' set the per-vertex colors of an object of class mesh3d
+#' @param x mesh3d
+#' @param colorvec vector of lenght \code{ncol(x$vb)} containing HEX rgb values for vertex colors.
+#' @return mesh with updated vertex colors
+#' @export
+setVertColor <- function(x,colorvec) {
+    if (ncol(x$vb) != length(colorvec))
+        stop("provide a color value for each vertex")
+    colfun <- function(x){x <- colorvec[x];return(x)}
+    x$material$color <- matrix(colfun(x$it),dim(x$it))
+    x$material$color[is.na(x$material$color)] <- #000000
+    return(x)
+}
