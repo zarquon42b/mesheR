983780ffe9ebb2aae11ed6a2e95e7fd25c1eaa43
zarquon42b <zarquon42@gmail.com>
2015-07-16 09:06:31 +0200

    bumped version

--- R/mesheR-package.R
+++ R/mesheR-package.R
@@ -5,8 +5,8 @@
 #' \tabular{ll}{
 #' Package: \tab mesheR\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.4.150601\cr
-#' Date: \tab 2015-06-01\cr
+#' Version: \tab 0.4.150716\cr
+#' Date: \tab 2015-07-16\cr
 #' License: \tab GPL\cr
 #' LazyLoad: \tab yes\cr }
 #' 
