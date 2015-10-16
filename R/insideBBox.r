16d123b780b1c778d5567f6bcb196f6e725914e5
zarquon42b <zarquon42@gmail.com>
2015-10-16 17:52:31 +0200

    added option pca to getMeshBox and fixed outsideBBox also added cropOutsideBBox

--- R/insideBBox.r
+++ R/insideBBox.r
@@ -3,8 +3,8 @@
 #' get vertex indices of vertices outside a bounding box
 #'
 #' @param x a k x 3 matrix or a triangular mesh of class "mesh3d"
-#' @param corners a 8 x 3 matrix defining the corners of a bounding box.
-#' @param outside logical: if TRUE the indices outside the box are returned, those insde
+#' @param corners a 8 x 3 matrix defining the corners of a bounding box, or a triangular mesh of class "mesh3d" representing the meshBox
+#' @param outside logical: if TRUE the indices outside the box are returned, those inside
 #' @return returns the indices of coordinates inside/outside the bounding box.
 #' @examples
 #' require(Rvcg)
@@ -26,12 +26,18 @@
 #' }
 #' @export
 outsideBBox <- function(x, corners, outside=TRUE) {
-    box <- makeBox(corners)
-    clost <- vcgClost(x,box,sign=T)
-    if (outside)
-        outside <- which(clost$quality > 0)
+    if (!inherits(corners,"mesh3d"))
+        box <- makeBox(corners)
     else
-        outside <- which(clost$quality < 0)
+        box <- corners
+    clost <- vcgClostKD(x,box,sign=T,k=16,weightnorm = F,threads = 1)
+    x$normals[1:3,] <- x$vb[1:3,]-clost$vb[1:3,]
+    test <- normcheck(clost,x)
+    #test <- clost$quality
+    if (outside)
+        outside <- which(test > pi/2)
+    else
+        outside <- which(test < pi/2)
     return(outside)
     
 }
@@ -40,13 +46,21 @@ outsideBBox <- function(x, corners, outside=TRUE) {
 #'
 #' create a bounding box as mesh
 #' @param corners a 8 x 3 matrix defining the corners of a bounding box. With first 4 rows containing the corners of one square and 5:8 their opposing counterparts.
+#' @importFrom Morpho invertFaces
+#' @importFrom Rvcg vcgRaySearch
 #' @export
 makeBox <- function(corners) {
     bbox4 <- list(vb = rbind(t(corners),1))
-    it <- c(1,2,4,4,3,1,1,5,2,2,5,6,7,8,6,6,5,7,7,3,4,4,8,7,1,3,7,7,5,1,2,6,4,4,6,8)
-    
+    it <- c(4,2,1,1,3,4,2,5,1,6,5,2,6,8,7,7,5,6,4,3,7,7,8,4,7,3,1,1,5,7,4,6,2,8,6,4)
     bbox4$it <- matrix(it,3,length(it)/3)
     class(bbox4) <- "mesh3d"
+    bbox4 <- vcgUpdateNormals(bbox4)
+    dircheck <- vcgRaySearch(bbox4,meshOffset(bbox4,-1e-5))
+    if (sum(dircheck$quality) > 0) {
+        print(1)
+        bbox4 <- invertFaces(bbox4)
+    }
+
     return(bbox4)
 }
 
@@ -56,6 +70,7 @@ makeBox <- function(corners) {
 #' @param mesh triangular mesh of class "mesh3d"
 #' @param tri logical: if TRUE, triangular mesh representing the bounding box is returned
 #' @param extend numeric: offset extending the bbox (or shrinking if negative)
+#' @param pca if TRUE, the box will be aligned by the principal axes.
 #' @return returns a matrix with the corners of the box or a triangular mesh if tri=TRUE.
 #' @examples
 #' require(Rvcg)
@@ -71,15 +86,39 @@ makeBox <- function(corners) {
 #' wire3d(humface)
 #' }
 #' @export
-getMeshBox <- function(mesh,tri=FALSE,extend=0) {
+getMeshBox <- function(mesh,tri=FALSE,extend=0,pca=TRUE) {
     vv2 <- vert2points(mesh)
-    pp <- prcomp(vv2)
-    ranges <- apply(pp$x,2,range)
+    if (pca) {
+        pp <- prcomp(vv2)
+        ranges <- apply(pp$x,2,range)
+    } else {
+        ranges <- apply(vv2,2,range)
+    }    
     ranges <- apply(ranges,2,extendrange,f=extend)
     mc <- as.matrix(expand.grid(ranges[,1],ranges[,2],ranges[,3]))
-    bbox <- sweep(mc%*%t(pp$rotation),2,-pp$center)
+    if (pca)
+        bbox <- sweep(mc%*%t(pp$rotation),2,-pp$center)
+    else
+        bbox <- mc
     if (tri)
         bbox <- makeBox(bbox)
     return(bbox)
 }
 
+#' remove parts of a mesh outside a bounding box defined by another mesh
+#'
+#' remove parts of a mesh outside a bounding box defined by another mesh
+#' @param mesh1 reference mesh of which the bounding box will be computed
+#' @param mesh2 mesh to be cropped
+#' @param extend numeric: offset extending the bbox (or shrinking if negative)
+#' @param pca if TRUE, the box will be aligned by the principal axes.
+#' @param outside logical: if TRUE the part outside the box is removed
+#' @return returns the cropped mesh2
+#' @export 
+cropOutsideBBox <- function(mesh1,mesh2,extend=0,pca=TRUE,outside=FALSE) {
+    bbox <- getMeshBox(mesh1,tri=TRUE,extend=extend, pca=pca)
+    outsidev <- outsideBBox(mesh2,bbox,outside=outside)
+    if (length(outsidev))
+        mesh2 <- Morpho::rmVertex(mesh2,outsidev)
+    return(mesh2)
+}
