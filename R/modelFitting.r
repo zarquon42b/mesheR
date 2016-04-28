351a3c0943b6b9d723273e20ad87626eef262ce6
zarquon42b <zarquon42@gmail.com>
2016-04-28 13:52:48 +0200

    added option to set penalty termin modelFitting for each iteration

--- R/modelFitting.r
+++ R/modelFitting.r
@@ -34,6 +34,21 @@ objectiveMSQ.grad <- function(x,clost,A,B,tarclost) {
 #' @param tarmesh a target mesh already aligned to the model
 #' @param iterations numbers of iterations to run
 #' @param lbfgs.iter integer:
+#' @param lbfgs.orthantwise_c integer or integer vector of length iterations. Contains coefficient(x) for the \code{L1} norm of variables. This parameter should be set to zero for standard minimization
+#' problems. Setting this parameter to a positive value
+#' activates Orthant-Wise Limited-memory Quasi-Newton (OWL-QN)
+#' method, which minimizes the objective function \code{F(x)}
+#' combined with the L1 norm \code{|x|} of the variables, \code{{F(x) + C
+#' |x|}}. This parameter is the coefficient for the \code{|x|}, i.e.,
+#' \code{C}. As the L1 norm \code{|x|} is not differentiable at zero, the
+#' library modifies function and gradient evaluations from a
+#' client program suitably. The default value is zero. Note that
+#' the objective function minimized by alternative packages
+#' (e.g., \code{glmnet}) is of the form : \code{F(x)/N + C |x|}, where \code{N}
+#' is the number of parameters. \code{lbfgs} does not divide the
+#' likelihood function by \code{N}. To achieve equivalence with
+#' \code{glmnet} result, take this difference of implementation into
+#' account.
 #' @param refdist maximal distance from model to reference to be considered
 #' @param tardist maximal distance from target to model to be considered
 #' @param rho numeric: allowed normal deviation of a point to be considered as corresponding.
@@ -61,11 +76,10 @@ objectiveMSQ.grad <- function(x,clost,A,B,tarclost) {
 #' download.file(url="https://github.com/marcelluethi/
 #' statismo-shaperegistration/raw/master/data/VSD002-lm.csv","./VSD002-lm.csv",method = "w")
 #' 
-#' ref <- read.vtk("VSD001_femur.vtk")
-#' tar <- read.vtk("VSD002_femur.vtk")
-#' ref.lm <- as.matrix(read.csv("VSD001-lm.csv",row.names=1))
-#' tar.lm <- as.matrix(read.csv("VSD002-lm.csv",row.names=1))
-#' mymod <- statismoModelFromRepresenter(ref,kernel=list(c(50,50)),ncomp = 100,isoScale = 0.1)
+#' ref.lm <- as.matrix(read.csv("VSD001-lm.csv",row.names=1,header = FALSE))
+#' tar.lm <- as.matrix(read.csv("VSD002-lm.csv",row.names=1,header = FALSE))
+#' Kernels <- SumKernels(GaussianKernel(50,50),IsoKernel(0.1,ref))
+#' mymod <- statismoModelFromRepresenter(ref,kernel=Kernels)
 #' mymodC <- RvtkStatismo::statismoConstrainModel(mymod,tar.lm,ref.lm,2)
 #' fit <- modelFitting(mymodC,tar,iterations = 15)
 #' #or without landmarks but instead with some icp steps
@@ -81,7 +95,7 @@ objectiveMSQ.grad <- function(x,clost,A,B,tarclost) {
 #' @note needs RvtkStatismo installed
 #' @importFrom lbfgs lbfgs
 #' @export
-modelFitting <- function(model, tarmesh, iterations=5,lbfgs.iter=5,symmetric=c(0,1,2),refdist=1e5,tardist=1e5,rho=pi/2,sdmax=NULL,mahaprob=c("none","chisq","dist"),initparams=NULL,k=50,threads=0,method="lbfgs",silent=FALSE,...) {
+modelFitting <- function(model, tarmesh, iterations=5,lbfgs.iter=5,lbfgs.orthantwise_c=0, symmetric=c(0,1,2),refdist=1e5,tardist=1e5,rho=pi/2,sdmax=NULL,mahaprob=c("none","chisq","dist"),initparams=NULL,k=50,threads=0,method="lbfgs",silent=FALSE,...) {
     if (!requireNamespace("RvtkStatismo"))
         stop("you need to install RvtkStatismo from https://github.com/zarquon42b/RvtkStatismo")
     if (!is.null(initparams)) {
@@ -94,6 +108,11 @@ modelFitting <- function(model, tarmesh, iterations=5,lbfgs.iter=5,symmetric=c(0
     } else {
         vars <- rep(0,length(RvtkStatismo::GetPCAVarianceVector(model)))
     }
+    if (length(lbfgs.orthantwise_c) == 1)
+        lbfgs.orthantwise_c <- rep(lbfgs.orthantwise_c,iterations)
+    else if (length(lbfgs.orthantwise_c) != iterations)
+        stop("lbfgs.orthantwise_c must be of the same length as iterations")
+    
     Aorig <- RvtkStatismo::GetPCABasisMatrix(model)
     A <- clost <- NULL
     B <- tarclost <- NULL
@@ -138,7 +157,7 @@ modelFitting <- function(model, tarmesh, iterations=5,lbfgs.iter=5,symmetric=c(0
         if (method != "lbfgs")
             out <- optim(par=vars,objectiveMSQ,objectiveMSQ.grad,A=A,clost=clost,B=B,tarclost=tarclost,method = method,...)
         else
-            out <- lbfgs(objectiveMSQ,objectiveMSQ.grad,vars=vars,A=A,clost=clost,B=B,tarclost=tarclost,max_iterations = lbfgs.iter,invisible=1,...)
+            out <- lbfgs(objectiveMSQ,objectiveMSQ.grad,vars=vars,A=A,clost=clost,B=B,tarclost=tarclost,max_iterations = lbfgs.iter,invisible=1,orthantwise_c=lbfgs.orthantwise_c[i],...)
         vars <- out$par
         if (!is.null(sdmax)) {
             vars <-  constrainParams(vars,sdmax=sdmax,mahaprob = mahaprob)
