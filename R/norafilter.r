c9e2b27542ba13b63752aeb058e6ec2459a0b8b4
zarquon42b <zarquon42@gmail.com>
2024-07-01 12:23:29 +0200

    NEW: read.ano.json to read NORA export landmarks

--- R/norafilter.r
+++ R/norafilter.r
@@ -41,3 +41,26 @@ write.ano.json <- function(x,filename=dataname,labels=dataname,size=1) {
     write_json(out,pretty=T,auto_unbox = T,filename,digits=NA,always_decimal=TRUE,null="list")
     
 }
+
+#' read landmarks from json file exported by NORA software
+#'
+#' read landmarks from json file exported by NORA software
+#' @param x  filename
+#' @export
+#' @importFrom jsonlite read_json
+read.ano.json <- function(x) {
+    xin <- read_json(x)
+    anno <- xin$annotations
+    outmat <- NULL
+    names <- NULL
+    for (i in 1:length(anno[[1]]$points)) {
+        out <- NULL
+        for (j in 1:3)
+            out[j] <- anno[[1]]$points[[i]]$coords[[j]]
+        
+    outmat <- rbind(outmat,out)
+        names <- c(names,anno[[1]]$points[[i]]$name)
+    }
+    rownames(outmat) <- names
+    return(outmat)
+}
